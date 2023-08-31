dc7db251d98a1fa4eafed20359c14fbc8819bd68
Emily Gonsales <emily.gonsales@aluno.senai.br>
2023-08-31 16:51:25 -0300

    Criando endpoint para buscar a lista de produtos

---
+++ loja/src/main/java/com/loja/TabelaProdutos.java
@@ -0,0 +1,40 @@
+package com.loja;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabelaProdutos {
+    List<Produto> produtos = new ArrayList<>();
+
+    /**
+     * No construtor a Lista de produtos
+     * será preenchida :)
+     */
+
+    public TabelaProdutos() {
+        Produto caneta = new Produto(1, "Caneta BIC", "Caneta BIC na cor azul", 1.99);
+        Produto borracha = new Produto(2, "Borracha branca", "Borracha branca comum", 3.99);
+        Produto lapis = new Produto(3, "Lapis Faber Castel", "Lapis Faber Castel Comum", 2.99);
+        Produto apontador = new Produto(4, "Apontador", "Apontador comum com reservátorio", 7.99);
+        Produto caderno = new Produto(5, "Caderno espiral", "Caderno espiral de 96 folhas", 15.99);
+
+        this.produtos.add(caneta);
+        this.produtos.add(borracha);
+        this.produtos.add(lapis);
+        this.produtos.add(apontador);
+        this.produtos.add(caderno);
+
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+    /**
+     * método que retorna todos os produtos da Lista
+     */
+
+    public List<Produto> buscarTodosOsProdutos(){
+        return this.getProdutos();
+    }
+}
